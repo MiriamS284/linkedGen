9468a33f6ef02035a13235032751e28b35c1d276
Miriam Sparbrod <miriam.sparbrod@gmail.com>
2024-05-02 12:13:11 +0200

    server configurations added

--- server/index.js
+++ server/index.js
@@ -15,7 +15,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: [
+    "http://localhost:5173",
+    "https://linked-21z6lp13a-miriams284s-projects.vercel.app",
+  ],
 };
 
 app.use(cors(corsOptions));
