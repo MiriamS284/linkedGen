7b34d37263c6b7a4dfa864e8f4aac3152003c3ae
Miriam Sparbrod <miriam.sparbrod@gmail.com>
2024-05-03 16:52:25 +0200

    environment variables setup

--- client/src/services/axiosClient.js
+++ client/src/services/axiosClient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_BASE_URL;
+const API_URL = "https://linkedgen-server.onrender.com";
 
 const api = axios.create({
   baseURL: `${API_URL}/api`,
